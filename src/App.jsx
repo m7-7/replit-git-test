9a897ef5cfb65cb8b753dfddc6623f267f1001e7
m7-7 <63880758+m7-7@users.noreply.github.com>
2025-06-17 10:04:32 +0000

    Work on remix m7-7

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <h1>Hello World!</h1>
-      <p>Welcome to my simple React app</p>
+      <p>Welcome to my React app</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
